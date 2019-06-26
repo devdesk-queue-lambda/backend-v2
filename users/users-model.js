08afe4b1ae1bda77944d2bd79b7f8847eff63a6b
joerlop <jj.erlich155@gmail.com>
2019-06-25 19:44:36 -0500

    Get users returns authType

--- users/users-model.js
+++ users/users-model.js
@@ -11,7 +11,7 @@ module.exports = {
 };
 
 function find() {
-  return db('users').select('id', 'username');
+  return db('users').select('id', 'username', "authType");
 }
 
 function findBy(filter) {
